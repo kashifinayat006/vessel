a552f4a22351d26ccf342340819571543ad980c1
vikingowl <christian@nachtigall.dev>
2026-01-02 20:36:03 +0100

    chore: bump version to 0.4.1

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -18,7 +18,7 @@ import (
 )
 
 // Version is set at build time via -ldflags, or defaults to dev
-var Version = "0.4.0"
+var Version = "0.4.1"
 
 func getEnvOrDefault(key, defaultValue string) string {
 	if value := os.Getenv(key); value != "" {
